4730fae437442a6fdc76d23d7da587f64e2561b0
lquicenog <95106528+lquicenog@users.noreply.github.com>
2022-07-02 21:21:18 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -30,14 +30,14 @@ def create_data_lake():
    
     import os
 
-    os.mkdir('../data_lake/')
-    parent_dir = '../data_lake/'
+    os.mkdir('./data_lake/')
+    parent_dir = './data_lake/'
     carpetas = ['landing', 'raw', 'cleansed', 'business']
     [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
-    parent_dir = '../data_lake/business/'
+    parent_dir = './data_lake/business/'
     carpetas = ['reports', 'features', 'forecasts']
     [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
-    parent_dir = '../data_lake/business/reports/'
+    parent_dir = './data_lake/business/reports/'
     directory = 'figures'
     os.mkdir(os.path.join(parent_dir, directory))
     
