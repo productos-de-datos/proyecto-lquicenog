2ea2d153c8e397e4b6e134dd18fa13bcd79f979a
lquicenog <95106528+lquicenog@users.noreply.github.com>
2022-07-02 20:09:19 -0500

    Add files via upload

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -1,11 +1,4 @@
 """
-Documentación:
-Este pipeline tiene como objetivo orquestar todas las funciones anteriormente realizadas, una vez se haya creado el datalake. 
-Es decir, que se ejecutan las funciones: ingest_data, transform_data, clean_data, compute_daily_prices and compute_monthly_prices. 
-"""
-
-
-"""
 Construya un pipeline de Luigi que:
 
 * Importe los datos xls
@@ -22,7 +15,7 @@ En luigi llame las funciones que ya creo.
 import luigi
 from luigi import Task, LocalTarget
 
-#Se cre una clase para llamar la función de importar datos (ingest_data)
+#Creamos una clase que llame la funcion de importar datos (ingest_data)
 class ingestar_datos(Task):
     def output(self):
         return LocalTarget('data_lake/landing/ingestar_datos_pipeline.txt')
@@ -33,7 +26,8 @@ class ingestar_datos(Task):
         with self.output().open('w') as archivos:
             ingest_data()
 
-#Se crea una clase para llamar la función que transforma los datos de xls y xlsx a csv (transform_data)
+#Creamos una clase que llame la funcion para tranformar los datos de xls a csv (transform_data), para
+#esto es necesario previamente tener la clase anterior ingestar_datos()
 class transformar_datos(Task):
     def requires(self):
         return ingestar_datos()
@@ -47,7 +41,8 @@ class transformar_datos(Task):
         with self.output().open('w') as archivos:
             transform_data()
 
-#Se crea una clase para llamar la función clean_data y crear un solo archivo tipo csv 
+#Creamos una clase que llame la funcion para limpiar los datos y hacer un solo csv (clean_data), para
+#esto es necesario previamente tener la clase anterior transformar_datos()
 class limpiar_datos(Task):
     def requires(self):
         return transformar_datos()
@@ -61,7 +56,8 @@ class limpiar_datos(Task):
         with self.output().open('w') as archivos:
             clean_data()
 
-#Se crea una clase para llamar la función compute_daily_prices y calcular el promedio de precios diario
+#Creamos una clase que llame la funcion computar el precio diario promedio (compute_daily_prices), para
+#esto es necesario previamente tener la clase anterior limpiar_datos()
 class precio_diario(Task):
     def requires(self):
         return limpiar_datos()
@@ -75,7 +71,9 @@ class precio_diario(Task):
         with self.output().open('w') as archivos:
             compute_daily_prices()
 
-#Se crea una clase para llamar la función compute_monthly_prices y calcular el promedio mensual de precios.
+#Creamos una clase que llame la funcion computar el precio diario promedio (compute_monthly_prices), para
+#esto es necesario previamente tener la clase anterior precio_diario()
+class precio_mensual(Task):
     def requires(self):
         return precio_diario()
 
