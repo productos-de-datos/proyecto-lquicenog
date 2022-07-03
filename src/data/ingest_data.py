cf7b1486a3be19fa0ac12ff824a63b223f91b2a7
lquicenog <95106528+lquicenog@users.noreply.github.com>
2022-07-02 19:08:00 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,4 +1,12 @@
 """
+Documentación:
+    La funcionalidad de ingest_data es descargar los archivos por año de los precios_bolsa_nacional. Estos archivos se descargan 
+    desde 1995 hasta 2021. Se debe tener presente que para los año 2016 y 2017 la extensión de los archivos cambia (xlsx); los demás
+    tiene extensión xls.
+"""
+
+
+"""
 Módulo de ingestión de datos.
 -------------------------------------------------------------------------------
 
@@ -15,12 +23,7 @@ def ingest_data():
     """
 
     import requests
-
-    #Observamos que algunos archivos del repositorio tienen extension .xls y otros .xlsx, definimos
-    #para cada caso como descargar el archivo.
-
-
-    #Ciclo for para recorrer el numero de años (archivos a descargar) segun su extension 
+ 
     for num in range(1995, 2022):
         if num in range(2016, 2018):
             url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(num)
